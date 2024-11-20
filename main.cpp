833417d11a1b6b5ba252e85f9a153e9ae53115c2
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-20 20:17:22 +0200

    ex_1 is done

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
+//Напишіть програму, яка обчислює суму цілих чисел від а до 500 (значення a вводиться з клавіатури).
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int user_num;
+    int user_number;
     int sum_number = 0;
 
-    do
-    {
-        cout << "Enter the number -> ";
-        cin >> user_num;
+    cout << "Enter the start range ->";
+    cin >> user_number;
 
-        sum_number += user_num;
-    } while (user_num != 0);
+    for (user_number; user_number <= 500; user_number++)
+    {
+        sum_number += user_number;
+    }
 
     cout << sum_number;
 
