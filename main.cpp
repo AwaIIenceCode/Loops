6f1a339cbd82b22e6bbc30cfa1a260b58c28d29a
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-20 19:56:04 +0200

    ex_3 is done

--- main.cpp
+++ main.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
 int main()
 {
     int start, finish;
+    int sum_number;
 
-    cout << "Enter the start range -> ";
+    cout << "Enter the start range ->";
     cin >> start;
+
     cout << "Enter the finish range -> ";
     cin >> finish;
 
-    if (start > finish)
-    {
-        int temp = start;
-        start = finish;
-        finish = temp;
-    }
-    cout << endl;
-
     for (start; start <= finish; start++)
     {
-        if (start % 2 == 0)
-        {
-            cout << "Number " << start << " event" << endl;
-        }
-
-        else if (start % 2 != 0)
-        {
-            cout << "Number " << start << " odd" << endl;
-        }
-
-        if (start % 7 == 0)
-        {
-            cout << "Number " << start << " multiple of 7" << endl;
-        }
+        sum_number += start;
     }
+
+    cout << sum_number << endl;
 
     return 0;
 }
