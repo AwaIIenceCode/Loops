0a30af9de6c34b8dd8c4a8b2f2939c88d32e80d1
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-24 00:42:53 +0200

    update

--- main.cpp
+++ main.cpp
@@ -6,16 +6,23 @@ int main()
 {
     int user_number;
     int result = 1;
+
     cout << "Enter the start range -> " << endl;
     cout << "Number must be >= 1" << endl;
     cin >> user_number;
 
-    for (user_number; user_number <= 20; user_number++)
+    if (user_number < 1 || user_number > 20)
     {
-        result *= user_number;
+        cout << "The number must be between 1 and 20!" << endl;
+        return 1;
     }
 
-    cout << "Dobutok= " << result;
+    for (int i = user_number; i <= 20; i++)
+    {
+        result *= i;
+    }
+
+    cout << "Product = " << result;
 
     return 0;
 }
